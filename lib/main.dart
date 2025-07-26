ede85a29eb59c158d91edd344bde3f2e9dae6eb9
ibukye <125569824+ibukye@users.noreply.github.com>
2025-07-26 16:12:23 +0800

    add onTap to handle isDone for all tasks

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,18 @@ class MyApp extends StatelessWidget {
 }
 
 
+
+// task class
+// 今まではタスクリストはただの文字列で何もタスクが終わったかどうか判断する要素がないのでクラスを作成してタスクリストの中の要素をこれに置き換える
+class TodoItem {
+  String title;   // task name
+  bool isDone;    // 完了状態 (true: 完了)
+
+  // 設計図から新しいタスクを作成するときに使う(constructor)
+  TodoItem({required this.title, this.isDone = false});
+}
+
+
 class TodoScreen extends StatefulWidget {
   // constructor
   const TodoScreen({super.key});
@@ -29,7 +41,7 @@ class _TodoScreenState extends State<TodoScreen> {
   final _controller = TextEditingController();
 
   // タスクを保持するリスト
-  final List<String> _todoList = [];
+  final List<TodoItem> _todoList = [];
 
   // タスクを追加するメソッド
   void _addTask() {
@@ -37,7 +49,8 @@ class _TodoScreenState extends State<TodoScreen> {
     if (_controller.text.isNotEmpty) {
       // setStateで状態を更新
       setState(() {
-        _todoList.add(_controller.text);
+        //_todoList.add(_controller.text);
+        _todoList.add(TodoItem(title: _controller.text)); // Titleに渡す
         _controller.clear();  // 追加したら入力欄を空にする
       });
     }
@@ -73,12 +86,26 @@ class _TodoScreenState extends State<TodoScreen> {
             ),
 
             // タスクリスト
+            // リストの項目をタップできるようにListTileにonTapというプロパティを追加
             Expanded(
               child: ListView.builder(
                 itemCount: _todoList.length,
                 itemBuilder: (context, index) {
                   // List Tileを使うときれいなリスト項目を作成できる
-                  return ListTile(title: Text(_todoList[index]));
+                  final todo = _todoList[index];  // 現在のタスクを取得
+                  return ListTile(
+                    onTap: () {
+                      setState(() {
+                        // isDoneの状態を反転させる
+                        todo.isDone = !todo.isDone;
+                      });
+                    },
+
+                    title: Text(
+                      todo.title,
+                      style: TextStyle(decoration: todo.isDone ? TextDecoration.lineThrough : TextDecoration.none)
+                    )
+                  );
                 }
               )
             )
